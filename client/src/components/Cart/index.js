800fb96b1073f128fc8fb8c8dc8812317314a223
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-18 12:27:29 -0700

    Components have been refactored the use redux.

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -12,7 +12,8 @@ import './style.css';
 const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
-  const [state, dispatch] = useStoreContext();
+  const dispatch = useDispatch();
+  const state = useSelector(state => state);
   const [getCheckout, { data }] = useLazyQuery(QUERY_CHECKOUT);
 
   useEffect(() => {
