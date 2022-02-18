f1b20b9bdec233d118c85e5046981c0c9ddefdbe
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-18 13:00:25 -0700

    Updated fille names.

---
+++ client/build/precache-manifest.f54a27a16416d67952739b989ce8afc4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9bc6565729dbfb6a642435facb826d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5cf37e2000d58c4981a9",
+    "url": "/static/css/main.e213c893.chunk.css"
+  },
+  {
+    "revision": "525cf1802e3630a4a1cd",
+    "url": "/static/js/2.fe746338.chunk.js"
+  },
+  {
+    "revision": "f333c948d07dc08e5747a9b83a79e23a",
+    "url": "/static/js/2.fe746338.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5cf37e2000d58c4981a9",
+    "url": "/static/js/main.b52384f4.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
