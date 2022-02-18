cdbf2072353db0ff3d222c988dc979f788f31989
Zachary Elliott <zacharyberdell@gmail.com>
2022-02-18 13:03:56 -0700

    App.js file changed.

--- client/src/App.js
+++ client/src/App.js
@@ -15,7 +15,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
 import { Provider } from 'react-redux';
-import store from './utils/reduxStore';
+import store from './utils/store';
 import Success from './pages/Success';
 import OrderHistory from './pages/OrderHistory';
 
